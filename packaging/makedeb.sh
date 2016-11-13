6ac1f3fa77a4ff407182ab4b0412719e472e7414
RogueM <chaosgen6@googlemail.com>
2016-11-13 17:14:40 +0000

    pimstaller 1.0.2 deb

--- packaging/makedeb.sh
+++ packaging/makedeb.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-gettools="yes"
-setup="yes"
-cleanup="yes"
+gettools="no" # if set to yes downloads the tools required
+setup="yes" # if set to yes populates program folder
+buildeb="yes" # if set to yes builds the deb files
+cleanup="yes" # if set to yes cleans up build files
 pkgfiles=( "build" "changes" "deb" "dsc" "tar.xz" )
-
-./makelog.sh
 
 if [ $gettools == "yes" ]; then
     sudo apt-get update && sudo apt-get install build-essential debhelper devscripts dh-make dput gnupg
 fi
 
 if [ $setup == "yes" ]; then
-    rm -R ../dashboard/debian &> /dev/null
-    cp -R ./debian/ ../dashboard/
+    rm -R ../program/build ../program/debian &> /dev/null
+    cp -R ./debian ../program/
 fi
 
-cd ../dashboard && debuild
+cd ../program
 
-for file in ${pkgfiles[@]}; do
-    mv ../*.$file ../packaging
-done
+if [ $buildeb == "yes" ]; then
+    debuild -aarmhf
+    for file in ${pkgfiles[@]}; do
+        rm ../packaging/*.$file &> /dev/null
+        mv ../*.$file ../packaging
+    done
+fi
 
 if [ $cleanup == "yes" ]; then
     debuild clean
-    rm -R ./debian &> /dev/null
+    rm -R ./build ./debian ./doc &> /dev/null
 fi
 
 exit 0
